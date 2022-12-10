371aaa2f988b0c4478219407fa254135ae697d6b
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-09 21:39:23 -0300

    Input values

--- app.py
+++ app.py
@@ -13,4 +13,10 @@ def divide(x, y):
     try:
         return x / y
     except:         
-        return print('Error! Não pode pode dividir por zero!')       
+        return print('Error! Não pode pode dividir por zero!')    
+
+valor1 = input('Digite o primeiro valor: ')           
+valor2 = input('Digite o segundo valor: ')   
+
+valor1_float = float(valor1)
+valor2_float = float(valor2)
